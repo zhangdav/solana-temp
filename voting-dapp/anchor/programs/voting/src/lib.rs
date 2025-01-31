1135cdad15d30a5e5ed7d449bda50f223a723f14
David Zhang <d12stan@hotmail.com>
2025-01-31 10:36:34 +0800

    update code

--- voting-dapp/anchor/programs/voting/src/lib.rs
+++ voting-dapp/anchor/programs/voting/src/lib.rs
@@ -30,10 +30,39 @@ pub mod voting {
         _poll_id: u64,
     ) -> Result<()> {
         let candidate = &mut ctx.accounts.candidate;
+        let poll = &mut ctx.accounts.poll;
+        poll.candidate_amount += 1;
         candidate.candidate_name = candidate_name;
         candidate.candidate_votes = 0;
         Ok(())
     }
+
+    pub fn vote(ctx: Context<Vote>, _candidate_name: String, _poll_id: u64) -> Result<()> {
+        let candidate = &mut ctx.accounts.candidate;
+        candidate.candidate_votes += 1;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(candidate_name: String, poll_id: u64)]
+pub struct Vote<'info> {
+    pub signer: Signer<'info>,
+
+    #[account(
+        seeds = [poll_id.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub poll: Account<'info, Poll>,
+
+    #[account(
+        mut,
+        seeds = [poll_id.to_le_bytes().as_ref(), candidate_name.as_bytes()],
+        bump
+    )]
+    pub candidate: Account<'info, Candidate>,
+
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
@@ -46,7 +75,6 @@ pub struct InitializeCandidate<'info> {
         seeds = [poll_id.to_le_bytes().as_ref()],
         bump,
     )]
-
     pub poll: Account<'info, Poll>,
 
     #[account(
@@ -56,7 +84,7 @@ pub struct InitializeCandidate<'info> {
         seeds = [poll_id.to_le_bytes().as_ref(), candidate_name.as_bytes()],
         bump
     )]
-    pub candidate: Account<'info, Candidate>, 
+    pub candidate: Account<'info, Candidate>,
 
     pub system_program: Program<'info, System>,
 }
@@ -76,10 +104,12 @@ pub struct InitializePoll<'info> {
     pub signer: Signer<'info>,
 
     #[account(
+        init,
+        payer = signer,
+        space = 8 + Poll::INIT_SPACE,
         seeds = [poll_id.to_le_bytes().as_ref()],
         bump,
     )]
-
     pub poll: Account<'info, Poll>,
 
     pub system_program: Program<'info, System>,
--- voting-dapp/anchor/tests/votingdapp.spec.ts
+++ voting-dapp/anchor/tests/votingdapp.spec.ts
@@ -1,6 +1,6 @@
 import * as anchor from '@coral-xyz/anchor'
 import {Program} from '@coral-xyz/anchor'
-import {Keypair, PublicKey} from '@solana/web3.js'
+import {PublicKey} from '@solana/web3.js'
 import {Voting} from '../target/types/voting'
 import {startAnchor} from 'anchor-bankrun'
 import { BankrunProvider } from 'anchor-bankrun'
@@ -11,12 +11,18 @@ const votingAddress = new PublicKey("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF
 
 describe('Voting', () => {
 
+  let context;
+  let provider;
+  let votingProgram: any;
+
+  beforeAll(async () => {
+    context = await startAnchor("", [{name: "voting", programId: votingAddress}], []);
+    provider = new BankrunProvider(context);
+
+    votingProgram = new Program<Voting>(IDL, provider);
+  })
+
   it('Initialize Poll', async () => {
-    const context = await startAnchor("", [{name: "voting", programId: votingAddress}], []);
-    const provider = new BankrunProvider(context);
-
-    const votingProgram = new Program<Voting>(IDL, provider);
-
     await votingProgram.methods.initializePoll(
       new anchor.BN(1),
       "What is your favorite type of peanut butter?",
@@ -36,5 +42,51 @@ describe('Voting', () => {
     expect(poll.pollId.toNumber()).toEqual(1);
     expect(poll.description).toEqual("What is your favorite type of peanut butter?");
     expect(poll.pollStart.toNumber()).toBeLessThan(poll.pollEnd.toNumber());
+  })
+
+  it("initialize candidate", async () => {
+    await votingProgram.methods.initializeCandidate(
+      "Smooth",
+      new anchor.BN(1),
+    ).rpc();
+
+    await votingProgram.methods.initializeCandidate(
+      "Crunchy",
+      new anchor.BN(1),
+    ).rpc();
+
+    const [crunchyAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, 'le', 8), Buffer.from("Crunchy")],
+      votingAddress,
+    )
+
+    const crunchyCandidate = await votingProgram.account.candidate.fetch(crunchyAddress);
+    console.log(crunchyCandidate);
+    expect(crunchyCandidate.candidateVotes.toNumber()).toEqual(0);
+
+    const [smoothAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, 'le', 8), Buffer.from("Smooth")],
+      votingAddress,
+    )
+
+    const smoothCandidate = await votingProgram.account.candidate.fetch(smoothAddress);
+    console.log(smoothCandidate);
+    expect(smoothCandidate.candidateVotes.toNumber()).toEqual(0);
+  })
+
+  it("vote", async () => {
+    await votingProgram.methods.vote(
+      "Smooth",
+      new anchor.BN(1),
+    ).rpc();
+
+    const [smoothAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, 'le', 8), Buffer.from("Smooth")],
+      votingAddress,
+    )
+
+    const smoothCandidate = await votingProgram.account.candidate.fetch(smoothAddress);
+    console.log(smoothCandidate);
+    expect(smoothCandidate.candidateVotes.toNumber()).toEqual(1);
   })
 })
