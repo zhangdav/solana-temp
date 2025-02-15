d615a9635ae9acc7767813f19baff13b320a167a
DAVID ZHANG <d12stan@hotmail.com>
2025-02-15 22:30:07 +0700

    Add swap directory

---
+++ swap/programs/swap/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("FoQqCBMAjTAv9xSt2kQFT2YDimeNBrAnGTL5tHskCurx");
+
+#[program]
+pub mod swap {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
