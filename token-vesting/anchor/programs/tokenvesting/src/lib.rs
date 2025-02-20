8cf35372bd7013fae613504a76239e966f18a027
DAVID ZHANG <d12stan@hotmail.com>
2025-02-20 21:17:00 +0700

    update code

--- token-vesting/anchor/programs/tokenvesting/src/lib.rs
+++ token-vesting/anchor/programs/tokenvesting/src/lib.rs
@@ -48,8 +48,43 @@ pub mod tokenvesting {
     }
 
     pub fn claim_token(ctx: Context<ClaimTokens>, company_name: String) -> Result<()> {
+      let employee_account = &mut ctx.accounts.employee_account;
+
+      let now = Clock::get()?.unix_timestamp;
+
+      if now < employee_account.cliff_time {
+        return Err(ErrorCode::ClaimNotAvailableYet.into())
+      }
+
+      let time_since_start = now.saturating_sub(employee_account.start_time);
+      let total_vesting_time = employee_account.end_time.saturating_sub(employee_account.start_time);
+
+      if total_vesting_time == 0 {
+        return Err(ErrorCode::InvalidVestingPeriod.into())
+      }
+
+      let vested_amount = if now >= employee_account.end_time {
+        employee_account.total_amount
+      } else {
+        match employee_account.total_amount.checked_mul(time_since_start as u64){
+          Some(product) => (
+            product / total_vesting_time as u64
+          ),
+          None => {
+            return Err(ErrorCode::CalculationOverflow.into())
+          }
+        }
+      };
+
+      let claimable_amount = vested_amount.saturating_sub(employee_account.total_withdrawn);
+
+      if claimable_amount == 0 {
+        return Err(ErrorCode::NothingToClaim.into())
+      }
+
       Ok(())
     }
+
 }
 
 #[derive(Accounts)]
@@ -168,4 +203,19 @@ pub struct EmployeeAccount {
     pub total_amount: u64,
     pub total_withdrawn: u64,
     pub bump: u8,
+}
+
+#[error_code]
+pub enum ErrorCode {
+  #[msg("Claiming is not available yet.")]
+  ClaimNotAvailableYet,
+
+  #[msg("Invalid vesting period")]
+  InvalidVestingPeriod,
+
+  #[msg("Calculation overflow")]
+  CalculationOverflow,
+
+  #[msg("Nothing to claim")]
+  NothingToClaim,
 }
