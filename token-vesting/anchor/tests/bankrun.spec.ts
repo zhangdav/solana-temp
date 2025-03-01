ba689190bf9f3f5e8f5bca762de5b2c964dc1032
DAVID ZHANG <d12stan@hotmail.com>
2025-03-01 22:33:58 +0700

    update code

--- token-vesting/anchor/tests/bankrun.spec.ts
+++ token-vesting/anchor/tests/bankrun.spec.ts
@@ -7,13 +7,19 @@ import IDL from "../target/idl/tokenvesting.json";
 import { Tokenvesting } from "../target/types/tokenvesting"; 
 import { SYSTEM_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/native/system";
 import { Program } from "@coral-xyz/anchor";
+import { createMint } from "@solana/spl-token";
+import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
 
 describe("Vesting Smart Contract Tests", () => {
   let beneficiary: Keypair;
   let context: ProgramTestContext;
   let provider: BankrunProvider;
   let program: Program<Tokenvesting>;
-  let BanksClient: BanksClient;
+  let banksClient: BanksClient;
+  let employer: Keypair;
+  let mint: PublicKey;
+  let beneficiaryProvider: BankrunProvider;
+  let program2: Program<Tokenvesting>;
 
   beforeAll(async () => {
     beneficiary = new anchor.web3.Keypair();
@@ -40,6 +46,16 @@ describe("Vesting Smart Contract Tests", () => {
 
     program = new Program<Tokenvesting>(IDL as Tokenvesting, provider)
 
-    BanksClient = context.banksClient;
+    banksClient = context.banksClient;
+
+    employer = provider.wallet.payer;
+
+    // @ts-expect-error - Type error in spl-token-bankrun dependency
+    mint = await createMint(banksClient, employer, employer.publicKey, null, 2);
+
+    beneficiaryProvider = new BankrunProvider(context);
+    beneficiaryProvider.wallet = new NodeWallet(beneficiary);
+
+    program2 = new Program<Tokenvesting>(IDL as Tokenvesting, beneficiaryProvider);
   })
 })     
