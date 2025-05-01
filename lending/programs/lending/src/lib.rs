451b7e0a8180a104a8f6fcb841a5c8f38066edd1
DAVID ZHANG <d12stan@hotmail.com>
2025-05-01 16:12:35 +0700

    add init user

--- lending/programs/lending/src/lib.rs
+++ lending/programs/lending/src/lib.rs
@@ -1,16 +1,24 @@
 use anchor_lang::prelude::*;
 
-mod state;
 mod instructions;
+mod state;
 
 declare_id!("B8McKr94AXQod2deREAaWgWibTzdxeW1FkhRg3GH651W");
 
 #[program]
 pub mod lending {
-    use anchor_lang::prelude::*;
     use crate::instructions::admin::InitBank;
-    
-    pub fn init_bank(_ctx: Context<InitBank>) -> Result<()> {
-        Ok(())
+    use anchor_lang::prelude::*;
+
+    pub fn init_bank(
+        ctx: Context<InitBank>,
+        liquidation_threshold: u64,
+        max_ltv: u64,
+    ) -> Result<()> {
+        process_init_bank(ctx, liquidation_threshold, max_ltv)
+    }
+
+    pub fn init_user(ctx: Context<InitUser>, usdc_address: Pubkey) -> Result<()> {
+        process_init_user(ctx, usdc_address)
     }
 }
