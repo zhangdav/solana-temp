b230cc6edb395d0611a1951428be455f4cbbefad
zhangdav <d12stan@hotmail.com>
2025-07-03 23:36:45 +0800

    feat: init lending

--- lending/programs/lending/src/lib.rs
+++ lending/programs/lending/src/lib.rs
@@ -14,35 +14,4 @@ pub mod lending {
     use crate::instructions::*;
     use anchor_lang::prelude::*;
 
-    pub fn init_bank(
-        ctx: Context<InitBank>,
-        liquidation_threshold: u64,
-        max_ltv: u64,
-    ) -> Result<()> {
-        process_init_bank(ctx, liquidation_threshold, max_ltv)
-    }
-
-    pub fn init_user(ctx: Context<InitUser>, usdc_address: Pubkey) -> Result<()> {
-        process_init_user(ctx, usdc_address)
-    }
-
-    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
-        process_deposit(ctx, amount)
-    }
-
-    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
-        process_withdraw(ctx, amount)
-    }
-
-    pub fn borrow(ctx: Context<Borrow>, amount: u64) -> Result<()> {
-        process_borrow(ctx, amount)
-    }
-
-    pub fn repay(ctx: Context<Repay>, amount: u64) -> Result<()> {
-        process_repay(ctx, amount)
-    }
-
-    pub fn liquidate(ctx: Context<Liquidate>) -> Result<()> {
-        process_liquidate(ctx)
-    }
 }
