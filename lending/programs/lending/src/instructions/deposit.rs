ecd06655aff8077287a0e55d7ed938ed3a5a8027
zhangdav <d12stan@hotmail.com>
2025-07-04 23:02:15 +0800

    feat: add deposit func

--- lending/programs/lending/src/instructions/deposit.rs
+++ lending/programs/lending/src/instructions/deposit.rs
@@ -42,9 +42,10 @@ pub struct Deposit<'info> {
         associated_token::token_program = token_program,
     )]
     pub user_token_account: InterfaceAccount<'info, TokenAccount>,
+
     pub token_program: Interface<'info, TokenInterface>,
-    pub system_program: Program<'info, System>,
     pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
 }
 
 pub fn process_deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
@@ -70,10 +71,7 @@ pub fn process_deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
     }
 
     let deposit_ratio = amount.checked_div(bank.total_deposits).unwrap();
-    let user_shares = bank
-        .total_deposit_shares
-        .checked_mul(deposit_ratio)
-        .unwrap();
+    let user_shares = bank.total_deposit_shares.checked_mul(deposit_ratio).unwrap();
 
     let user = &mut ctx.accounts.user_account;
 
@@ -81,7 +79,7 @@ pub fn process_deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
         key if key == user.usdc_address => {
             user.deposited_usdc += amount;
             user.deposited_usdc_shares += user_shares;
-        }
+        },
         _ => {
             user.deposited_sol += amount;
             user.deposited_sol_shares += user_shares;
@@ -90,8 +88,6 @@ pub fn process_deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
 
     bank.total_deposits += amount;
     bank.total_deposit_shares += user_shares;
-
-    user.last_updated = Clock::get()?.unix_timestamp;
 
     Ok(())
 }
--- lending/programs/lending/src/lib.rs
+++ lending/programs/lending/src/lib.rs
@@ -17,4 +17,8 @@ pub mod lending {
     pub fn init_user(ctx: Context<InitUser>, usdc_address: Pubkey) -> Result<()> {
         process_init_user(ctx, usdc_address)
     }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        process_deposit(ctx, amount)
+    }
 }
