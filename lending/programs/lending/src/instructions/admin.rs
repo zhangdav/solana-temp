3c66c468d0133d6c598a92884a536b5bab0c81bb
zhangdav <d12stan@hotmail.com>
2025-07-04 21:20:55 +0800

    feat: add init_user func

--- lending/programs/lending/src/instructions/admin.rs
+++ lending/programs/lending/src/instructions/admin.rs
@@ -33,12 +33,36 @@ pub struct InitBank<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+pub struct InitUser<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + User::INIT_SPACE,
+        seeds = [signer.key().as_ref()],
+        bump,
+    )]
+    pub user_account: Account<'info, User>,
+    pub system_program: Program<'info, System>,
+}
+
 pub fn process_init_bank(ctx: Context<InitBank>, liquidation_threshold: u64, max_ltv: u64) -> Result<()> {
     let bank = &mut ctx.accounts.bank;
     bank.mint_address = ctx.accounts.mint.key();
     bank.authority = ctx.accounts.signer.key();
     bank.liquidation_threshold = liquidation_threshold;
     bank.max_ltv = max_ltv;
-    
+
+    Ok(())
+}
+
+pub fn process_init_user(ctx: Context<InitUser>, usdc_address: Pubkey) -> Result<()> {
+    let user_account: &mut Account<'_, User> = &mut ctx.accounts.user_account;
+    user_account.owner = ctx.accounts.signer.key();
+    user_account.usdc_address = usdc_address;
+
     Ok(())
 }
--- lending/programs/lending/src/lib.rs
+++ lending/programs/lending/src/lib.rs
@@ -13,4 +13,8 @@ pub mod lending {
     pub fn init_bank(ctx: Context<InitBank>, liquidation_threshold: u64, max_ltv: u64) -> Result<()> {
         process_init_bank(ctx, liquidation_threshold, max_ltv)
     }
+
+    pub fn init_user(ctx: Context<InitUser>, usdc_address: Pubkey) -> Result<()> {
+        process_init_user(ctx, usdc_address)
+    }
 }
