ff9c24d78d3b709ab0fe7a061399bea84d37f5da
zhangdav <d12stan@hotmail.com>
2025-07-21 15:27:59 +0800

    feat: add update_config interface

--- stablecoin/programs/stablecoin/src/lib.rs
+++ stablecoin/programs/stablecoin/src/lib.rs
@@ -17,4 +17,8 @@ pub mod stablecoin {
     pub fn initialize_config(ctx: Context<InitializeConfig>) -> Result<()> {
         process_initialize_config(ctx)
     }
+
+    pub fn update_config(ctx: Context<UpdateConfig>, min_health_factor: u64) -> Result<()> {
+        process_update_config(ctx, min_health_factor)
+    }
 }
